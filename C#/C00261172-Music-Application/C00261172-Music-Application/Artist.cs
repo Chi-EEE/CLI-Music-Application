a8373ab70ea13c58e0e7c2e481c07f120ace2522
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-11-30 22:48:51 +0000

    Added Artist and Song for C#

---
+++ C#/C00261172-Music-Application/C00261172-Music-Application/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C00261172_Music_Application
+{
+    internal class Artist
+    {
+        public Artist(string name) {
+            this.name = name;
+        }
+        public static bool operator ==(Artist artistLeft, Artist artistRight)
+        {
+            return artistLeft.name == artistRight.name;
+        }
+        public static bool operator !=(Artist artistLeft, Artist artistRight)
+        {
+            return artistLeft.name != artistRight.name;
+        }
+        public string name { get; set; }
+    }
+}
---
+++ C#/C00261172-Music-Application/C00261172-Music-Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C00261172_Music_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
